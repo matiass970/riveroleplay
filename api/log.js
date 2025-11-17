04d51b1dc0d5c4842937183f77c7988789636e22
matiass970 <koctala77@gmail.com>
2025-11-17 17:56:50 +0300

    Update log.js

--- api/log.js
+++ api/log.js
@@ -1,5 +1,9 @@
 import { MongoClient } from "mongodb";
 
+export const config = {
+  runtime: "nodejs"
+};
+
 const uri = process.env.MONGO_URL;
 let client;
 
@@ -13,13 +17,18 @@ export default async function handler(req, res) {
     const db = client.db("rivero");
     const col = db.collection("visitors");
 
-    const ip = req.headers["x-forwarded-for"]?.split(",")[0]
-            || req.socket.remoteAddress;
+    const ip =
+      req.headers["x-forwarded-for"]?.split(",")[0] ||
+      req.socket.remoteAddress;
 
     const userAgent = req.headers["user-agent"];
     const date = new Date();
 
-    await col.insertOne({ ip, userAgent, date });
+    await col.insertOne({
+      ip: ip,
+      userAgent: userAgent,
+      date: date,
+    });
 
     res.status(200).json({ ok: true });
   } catch (err) {
